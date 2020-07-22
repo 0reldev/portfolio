c5e7b01a122bb1c69ef1cb87a1f1c1e117810eef
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-07-22 14:19:28 +0200

    First commit.

---
+++ src/test/java/com/reldev/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reldev.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
