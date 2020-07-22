ba1261949f235107526afc0f72e4a6c157f882b0
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-07-22 14:58:09 +0200

    Creation of a main controller.

---
+++ src/main/java/com/reldev/website/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.reldev.website.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getIndex() {
+
+        return "/index";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin() {
+
+        return "/admin";
+    }
+}
