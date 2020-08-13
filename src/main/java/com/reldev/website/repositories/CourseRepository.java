82793f88f6f6dcd41736bbf01741d36f546fad7d
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-08-13 03:37:46 +0200

    Gestion des formations

--- src/main/java/com/reldev/website/repositories/CourseRepository.java
+++ src/main/java/com/reldev/website/repositories/CourseRepository.java
@@ -1,7 +1,6 @@
 package com.reldev.website.repositories;
 
 import com.reldev.website.entities.Course;
-import com.reldev.website.entities.Experience;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
